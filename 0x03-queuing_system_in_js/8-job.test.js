979abf162d06e7e400b643a8f66167d8fc05cf58
Antoniomary <amanso.totti@yahoo.com>
2024-10-12 05:08:09 +0000

    8-job.test.js

---
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -0,0 +1,46 @@
+import kue from 'kue';
+import createPushNotificationsJobs from './8-job.js';
+import { expect } from 'chai';
+
+describe('createPushNotificationsJobs', () => {
+  let queue;
+
+  before(() => {
+    queue = kue.createQueue();
+    queue.testMode.enter();
+  });
+
+  afterEach(() => {
+    queue.testMode.clear();
+  });
+
+  after(() => {
+    queue.testMode.exit()
+  });
+
+  it('throw an error if jobs is not an array', () => {
+    expect(() => createPushNotificationsJobs('not an array', queue)).to.throw(
+      'Jobs is not an array'
+    );
+  });
+
+  it('create jobs in the queue', () => {
+    const jobs = [
+      {
+        phoneNumber: '4153518780',
+        message: 'This is the code 1234 to verify your account',
+      },
+      {
+        phoneNumber: '4153518781',
+        message: 'This is the code 5678 to verify your account',
+      },
+    ];
+
+    createPushNotificationsJobs(jobs, queue);
+
+    expect(queue.testMode.jobs.length).to.equal(2);
+    expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
+    expect(queue.testMode.jobs[0].data).to.eql(jobs[0]);
+    expect(queue.testMode.jobs[1].data).to.eql(jobs[1]);
+  });
+});
